7b7359372e6921e41dfebffb8f2ef5f71202faab
Eric Nguyen <ericnguyen7575@gmail.com>
2025-11-24 00:55:08 -0800

    Fixed up skeleton

--- search.py
+++ search.py
@@ -2,51 +2,12 @@ import random
 
 RANDOM_SEED = 42  
 
-#Stub function
-def rand_eval(feature_set):
-    return random.uniform(0, 100)
+def rand_eval():
+    #TODO: Implement a random evaluation function
+    return 
     
-def forward_selection(total_features):
-
-    curr_features = [] # Start with no features
-    
-    init_accuracy = rand_eval(curr_features) # Initial state
-    
-    # Track best found so far
-    best_ovr_accuracy = init_accuracy
-    best_ovr_features = list(curr_features)
-    
-    print(f"Initial: {init_accuracy:.1f}%")
-    print("Beginning search.\n")
-    
-    for i in range(total_features):
-        best_accuracy_this_level = -1
-        feature_to_add_this_level = -1
-        
-        # Iterate through all features not in set
-        for feature in range(1, total_features + 1):
-            if feature not in curr_features:
-                temp_features = curr_features + [feature]
-                accuracy = rand_eval(temp_features)
-                
-                print(set(temp_features), accuracy)
-                
-                if accuracy > best_accuracy_this_level:
-                    best_accuracy_this_level = accuracy
-                    feature_to_add_this_level = feature
-        
-        # Keep adding even if accuracy decreases (Search all subsets)
-        if feature_to_add_this_level != -1:
-            curr_features.append(feature_to_add_this_level)
-            
-            if best_accuracy_this_level > best_ovr_accuracy:
-                best_ovr_accuracy = best_accuracy_this_level
-                best_ovr_features = list(curr_features)
-                
-    print(set(best_ovr_features), best_ovr_accuracy)
-
-def backward_elimination():
-    #TODO: Implement the backward elimination algorithm
+def forward_selection():
+    #TODO: Implement the forward selection algorithm
     return
 
 def backward_elimination():
@@ -75,7 +36,8 @@ def main():
 
     choice = input("\nEnter choice [1-2]: ").strip()
     if choice == "1":
-        forward_selection(total_features)
+        #TODO: Call the forward selection function
+        forward_selection()
     elif choice == "2":
         #TODO: Call the backward elimination function
         backward_elimination()
@@ -84,4 +46,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
