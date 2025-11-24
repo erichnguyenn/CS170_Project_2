66d48b56d02b98473a279a8f2b919c6fdfe1500f
Eric Nguyen <ericnguyen7575@gmail.com>
2025-11-24 01:04:33 -0800

    Added trace text forw. sel. output

--- search.py
+++ search.py
@@ -2,13 +2,51 @@ import random
 
 RANDOM_SEED = 42  
 
-def rand_eval():
-    #TODO: Implement a random evaluation function
-    return 
+#Stub function
+def rand_eval(feature_set):
+    return random.uniform(0, 100)
     
-def forward_selection():
-    #TODO: Implement the forward selection algorithm
-    return
+def forward_selection(total_features):
+
+    curr_features = [] # Start with no features
+    
+    init_accuracy = rand_eval(curr_features) # Initial state
+    
+    # Track best found so far
+    best_ovr_accuracy = init_accuracy
+    best_ovr_features = list(curr_features)
+    
+    print(f"Using no features and \"random\" evaluation, I get an accuracy of {init_accuracy:.1f}%")
+    print("Beginning search.\n")
+    
+    for i in range(total_features):
+        best_accuracy_this_level = -1
+        feature_to_add_this_level = -1
+        
+        # Iterate through all features not in set
+        for feature in range(1, total_features + 1):
+            if feature not in curr_features:
+                temp_features = curr_features + [feature]
+                accuracy = rand_eval(temp_features)
+                
+                print(f"\tUsing feature(s) {set(temp_features)} accuracy is {accuracy:.1f}%")
+                
+                if accuracy > best_accuracy_this_level:
+                    best_accuracy_this_level = accuracy
+                    feature_to_add_this_level = feature
+        
+        # Keep adding even if accuracy decreases (Search all subsets)
+        if feature_to_add_this_level != -1:
+            curr_features.append(feature_to_add_this_level)
+            print(f"\nFeature set {set(curr_features)} was best, accuracy is {best_accuracy_this_level:.1f}%")
+
+            if best_accuracy_this_level > best_ovr_accuracy:
+                best_ovr_accuracy = best_accuracy_this_level
+                best_ovr_features = list(curr_features)
+            else:
+                print("(Warning, Accuracy has decreased!)")
+                
+    print(f"\nFinished search!! The best feature subset is {set(best_ovr_features)}, which has an accuracy of {best_ovr_accuracy:.1f}%")
 
 def backward_elimination():
     #TODO: Implement the backward elimination algorithm
@@ -36,8 +74,7 @@ def main():
 
     choice = input("\nEnter choice [1-2]: ").strip()
     if choice == "1":
-        #TODO: Call the forward selection function
-        forward_selection()
+        forward_selection(total_features)
     elif choice == "2":
         #TODO: Call the backward elimination function
         backward_elimination()
